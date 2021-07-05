3ead1d29cfd169eb53c52e039239e686ab743b60
ryan.cho <ryan-cho-km@kinemaster.com>
2021-07-05 15:54:41 +0900

    add initial project

---
+++ SwiftUI-Samples/SwiftUI_SamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SamplesApp.swift
+//  SwiftUI-Samples
+//
+//  Created by RYAN on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
